9b5afdce2b201cc04707608bdae6a9897f0974f0
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-12-20 09:07:08 +0100

    Loops opgave lavet.

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -26,22 +30,33 @@ public class Loops {
      * Viser en liste med brugere i konsollen
      */
     public void printBrugereArr() {
-
-        // TODO Din kode her
+        for (int i = 0; i < getBrugere().length; i++) {
+            System.out.print(getBrugere()[i] + " ");
+        }
+        System.out.println();
     }
 
     /**
      * Viser en liste med brugere i konsollen i omvendt rækkefølge
      */
     public void printBrugereReverseArr() {
+        String[] brugere = getBrugere();
 
-        // TODO Din kode her
-
+        Arrays.sort(brugere, Collections.reverseOrder());
+        for (int i = 0; i < brugere.length; i++) {
+            System.out.print(brugere[i] + " ");
+        }
+        System.out.println();
     }
 
     public void waitingFor0() {
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
 
-        // TODO Din kode her
- 
+        while (input != 0) {
+            System.out.print("Venter på 0! Skriv et tal... ");
+            input = scanner.nextInt();
+        }
+        System.out.println("Du har tastet 0. Så det er slut!");
     }
 }
