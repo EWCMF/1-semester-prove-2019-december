8c402d1c35f2696f583859dc78715033c4772a47
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-12-20 11:05:09 +0100

    Database lavet.

---
+++ database/queries.sql
@@ -0,0 +1,9 @@
+﻿SELECT * FROM quiztable where sporgsmaal LIKE "%hedder%"
+
+SELECT * FROM quiztable ORDER BY svar DESC LIMIT 2
+
+UPDATE quiztable set sporgsmaal = "Hvem er programming wiz på EASJ?" where svar = "András"
+
+INSERT INTO quiztable (sporgsmaal, svar) VALUES ("Hvem har initialerne TGH?", "Tommy")
+
+DELETE FROM quiztable WHERE svar like "to%"
