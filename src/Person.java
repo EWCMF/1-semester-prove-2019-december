b6a06375340dbe7b881ebe8f71b01ee4c68eac18
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-12-20 09:22:03 +0100

    Person opgave lavet.

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -9,24 +11,24 @@ public class Person {
 
     // TODO Ret nedenstående datatyper
     String navn; // Test Testesen
-    String alder; // 18
+    int alder; // 18
     String by; // Næstved
-    String postNummer; // 4700
+    int postNummer; // 4700
     String vej; // Femøvej
     String husnummer; // 3B
     String nationalitet; // Dansk
     String fritidsStringeresser; // Snitning, LOL, strikke
     String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
-    String hojde; // 1.56 m
-    String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
-    String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
-    String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
+    double hojde; // 1.56 m
+    int vaegt;  // 76 kg
+    double karaktergennemsnit; // 5.33
+    int eksamensKarakterer; // 7, 12, -3, 4, 2, 10
+    Date opdateretDato; // Sun Jan 08 00:00:00 CET 1978
 
     // TODO Vis eksempler på typecasting
     @Override
     public String toString() {
-        return "Navn: " + navn + "By" + by;
+        return "Navn: " + navn + "\n" + "By: " + by + "\n" + "Karaktergennemsnit: " + (int) karaktergennemsnit + "\n" + "Dato: " + opdateretDato.toString();
     }
 
     public static void main(String[] args) {
@@ -34,7 +36,20 @@ public class Person {
         Person person = new Person();
         person.navn = "Test Testesen";
         person.by = "Næstved";
-        System.out.println();
+        person.karaktergennemsnit = 5.6;
+        person.opdateretDato = new Date(100000000000L);
+        System.out.println(person.toString());
+
+        //Double lavet til int. Vist med sum metoden fra Quickmaths
+
+        person.hojde = 1.98;
+
+        // her bliver a 1 da der rundes ned.
+        int a = (int) person.hojde;
+        int b = 5;
+
+        Quickmaths quickmaths = new Quickmaths();
+        System.out.println("Sum af a og b: " + quickmaths.sum(a, b));
     }
 
 }
